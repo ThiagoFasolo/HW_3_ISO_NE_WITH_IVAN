74b58680e6e3c830d97823d8fa53cf6a3fa3c973
ThiagoFasolo <thiagodfasolo@gmail.com>
2024-10-12 18:18:50 -0400

    Basic Table

---
+++ Basic_Table.py
@@ -0,0 +1,16 @@
+import plotly.graph_objects as go
+
+def create_table(data):
+    # Creates table using the data
+    figure = go.Figure(data=[go.Table(
+        # Builds and Styles the first row (titles) of the table
+        header=dict(values=list(data.columns),
+                    fill_color="grey",
+                    align = 'left'),
+        cells = dict(values=[data[columns] for columns in data.columns],
+                     fill_color="lightgrey",
+                     align='left'))
+    ])
+    figure.show()
+
+create_table(<insert_data>)
