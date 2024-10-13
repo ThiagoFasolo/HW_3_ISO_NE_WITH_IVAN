df3d3c9005a0d325cefdf1aea53a299f67f1c866
Ivan Sapozhnikov <sapozhnikov.i@northeastern.edu>
2024-10-13 16:52:04 -0400

    Scrap this but test

--- API_Request.py
+++ API_Request.py
@@ -1,19 +1,67 @@
 import requests
+from requests.auth import HTTPBasicAuth
+import json
+import pandas as pd
+from pandas import json_normalize
+from datetime import datetime, timedelta
 
-# Replace with your credentials
-username = 'sapozhnikov.i@northeastern.edu'
-password = 'Shark3Gum'
+def request_ISO(application = 'genfuelmix/current'):
+    '''
+    :param application: the application as specified by ISO
+    :return: a dicionary (usually) from that pull
+    '''
+    # Credentials
+    username = 'sapozhnikov.i@northeastern.edu'
+    password = 'Shark3Gum'
 
-# Example API endpoint (use .json for JSON response)
-url = 'https://webservices.iso-ne.com/api/v1.1/hourlysysload/current'
+    # Make URL
+    base_url = 'https://webservices.iso-ne.com/api/v1.1'
+    endpoint = f'/{application}.json'
+    # Full URL
+    url = base_url + endpoint
 
-response = requests.get(url, auth=(username, password))
+    # Make the request
+    response = requests.get(url, auth=HTTPBasicAuth(username, password))
 
-if response.status_code == 200:
-    try:
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Handle JSON response
         data = response.json()
-    except ValueError:
-        print("Error decoding JSON response.")
-else:
-    print(f"Request failed with status code {response.status_code}")
-    print(response.text)  # To see the raw response
+        # Unncomment to dump json
+        # print(json.dumps(data, indent=2))
+        return data
+    else:
+        print('Failed to retrieve data:', response.status_code, f'from {url}')
+
+def request_ISO_genfuelmix_day(date):
+    '''
+    input a date in format YYYYMMDD
+    return a dataframe of Genfuelmix data for that date
+    '''
+    # Use generic pull
+    data = request_ISO(application=f'genfuelmix/day/{date}')
+    # Return Dataframe instead of JSON
+    df = json_normalize(data['GenFuelMixes']['GenFuelMix'])
+    return df
+def request_ISO_genfuelmix_daterange(beg_date, end_date):
+    '''
+    :param beg_date: in format YYYYMMDD
+    :param end_date: in format YYYYMMDD
+    :return: dataframe with Genfuelmix data for that date range
+    '''
+    data_frames= []
+
+    # Convert the date strings to datetime objects
+    start_date = datetime.strptime(str(beg_date), '%Y%m%d')
+    end_date = datetime.strptime(str(end_date), '%Y%m%d')
+
+    # Initialize the current date to start date
+    current_date = start_date
+    while current_date <= end_date:
+        df = request_ISO_genfuelmix_day(current_date.strftime('%Y%m%d'))
+        current_date += timedelta(days=1)  # Increment the day by one
+
+        data_frames.append(df)
+
+    final_df = pd.concat(data_frames, ignore_index=True)
+    return final_df
